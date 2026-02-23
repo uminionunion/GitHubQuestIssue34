375a2470e084c2fbc07e73eef3d36051ec4f8963
Uminion <lahlalisalem@icloud.com>
2026-02-23 16:00:37 -0500

    changing uHub button's countdown from red to green

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -191,7 +191,7 @@ useEffect(() => {
           <Button onClick={handleOpenModalManually} className="relative">
             uHub
             {countdown !== null && (
-              <div className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
+              <div className="absolute -top-2 -right-2 bg-green-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
                 {countdown}
               </div>
             )}
