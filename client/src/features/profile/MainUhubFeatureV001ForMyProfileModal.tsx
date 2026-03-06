ddbd1be70b11cc84c13d9ca2cc2b803d1e17b396
Uminion <lahlalisalem@icloud.com>
2026-03-06 04:41:57 -0500

    Testing 3.1.5

--- client/src/features/profile/MainUhubFeatureV001ForMyProfileModal.tsx
+++ client/src/features/profile/MainUhubFeatureV001ForMyProfileModal.tsx
@@ -3108,8 +3108,8 @@ const getRandomizedProducts = (products: Product[]): Product[] => {
   {/* CENTER SECTION */}
   <div id="MainUhubFeatureV001ForMyProfileSettingsCenterCenterSection" className="p-2 md:p-4 overflow-y-auto" style={{ 
     width: window.innerWidth < 768 ? '100%' : `${centerWidthDesktop}%`,
-      height: '100vh',
-  maxHeight: '60000px',
+      height: '300vh',
+  maxHeight: '600000px',
     display: 'flex',
     flexDirection: 'column'
   }}>
